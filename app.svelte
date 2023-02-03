e5a5c1bd12b9b6fdc3c145c56ffb10e8151c32fd
NKGM-DV <122536953+NKGM-DV@users.noreply.github.com>
2023-02-02 21:15:54 -0300

    app.svelte

---
+++ app.svelte
@@ -0,0 +1,16 @@
+<script>
+	import Jogo from './Jogar.svelte'
+	import Sobre from './Sobre.svelte'
+	import Menu from './Menu.svelte'
+
+
+	import { estado } from './Estado.js'
+</script>
+
+{#if $estado === 'menu'}
+	<Menu/>
+{:else if $estado === 'sobre'}
+	<Sobre/>
+{:else if $estado === 'jogar'}
+	<Jogo/>
+{/if}
