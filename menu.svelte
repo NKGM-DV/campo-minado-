3bfe4f28ff68f7e0a3d727f6b223cdf05664e082
NKGM-DV <122536953+NKGM-DV@users.noreply.github.com>
2023-02-02 21:13:37 -0300

    menu.svelte

---
+++ menu.svelte
@@ -0,0 +1,23 @@
+<svelte:head>
+	<link rel="stylesheet" href="/styles/menu.css">
+</svelte:head>
+
+<script>
+	import { estado } from './Estado.js'
+	import { trocarEstadoDoJogo } from './Estado.js'
+</script>
+
+<h1>
+	CAMPO MINADO
+</h1>
+
+<!-- svelte-ignore a11y-click-events-have-key-events -->
+<div class='menu' on:click={() => trocarEstadoDoJogo('jogar')}>
+	Jogar
+</div>
+
+<!-- svelte-ignore a11y-click-events-have-key-events -->
+<div class='menu' on:click={() => trocarEstadoDoJogo('sobre')}>
+	Sobre
+</div>
+<!-- svelte-ignore a11y-click-events-have-key-events -->
