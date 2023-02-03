81d8772ad447c24700aed9a326e4cad3e6384ee1
NKGM-DV <122536953+NKGM-DV@users.noreply.github.com>
2023-02-02 21:12:42 -0300

    sobre.svelte

---
+++ sobre.svelte
@@ -0,0 +1,34 @@
+<svelte:head>
+	<link rel="stylesheet" href="/styles/sobre.css">
+</svelte:head>
+
+<script>
+	import VoltarMenu from './VoltarMenu.svelte'
+</script>
+
+<h1>DESENVOLVEDORES</h1>
+
+
+<p>
+		Anderson de Oliveira Ricardo  ➣ aor3@discente.ifpe.edu.br
+</p>
+
+<p>
+	    Eudes Lima Da Silva  ➣ elds2@discente.ifpe.edu.br
+</p>
+
+<p>
+	    Evandro Henrique de Oliveira  ➣ eho1@discente.ifpe.edu.br
+</p>
+
+<p>
+	    Lucas Pedro Sá da Silva  ➣ lpss1@discente.ifpe.edu.br
+</p>
+
+<p>
+	   Nicole Kathlen Gomes Mota  ➣  nkgm@discente.ifpe.edu.br
+</p>
+
+
+
+<VoltarMenu/>
